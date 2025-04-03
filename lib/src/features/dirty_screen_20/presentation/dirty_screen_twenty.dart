e1b05d9985d1eb46a0a2a236fb0cafdaa0bb89f4
Leon Bösche <leonboesche1999@gmail.com>
2025-04-03 15:06:54 +0200

    dirty_screen_20

--- lib/src/features/dirty_screen_20/presentation/dirty_screen_twenty.dart
+++ lib/src/features/dirty_screen_20/presentation/dirty_screen_twenty.dart
@@ -10,69 +10,64 @@ class DirtyScreenTwenty extends StatelessWidget {
         title: const Text("Dirty Screen #20"),
       ),
       body: ListView(
-        children: const [
-          ListTile(
-            leading: Badge(
-              label: Text(
-                '1',
-                style: TextStyle(color: Colors.white),
-              ),
-              child: CircleAvatar(
-                backgroundImage: NetworkImage(
-                    'https://randomuser.me/api/portraits/men/11.jpg'),
-              ),
-            ),
-            title: Text('Michael Scott'),
-            subtitle: Text('Regional Manager'),
-            trailing: Icon(Icons.more_vert),
-          ),
-          ListTile(
-            leading: Badge(
-              label: Text(
-                '4',
-                style: TextStyle(color: Colors.white),
-              ),
-              child: CircleAvatar(
-                backgroundImage: NetworkImage(
-                    'https://randomuser.me/api/portraits/women/22.jpg'),
-              ),
-            ),
-            title: Text('Pam Beesly'),
-            subtitle: Text('Receptionist'),
-            trailing: Icon(Icons.more_vert),
-          ),
-          ListTile(
-            leading: Badge(
-              label: Text(
-                '99+',
-                style: TextStyle(color: Colors.white),
-              ),
-              child: CircleAvatar(
-                backgroundImage: NetworkImage(
-                    'https://randomuser.me/api/portraits/men/33.jpg'),
-              ),
-            ),
-            title: Text('Jim Halpert'),
-            subtitle: Text('Sales Representative'),
-            trailing: Icon(Icons.more_vert),
-          ),
-          ListTile(
-            leading: Badge(
-              label: Text(
-                '2',
-                style: TextStyle(color: Colors.white),
-              ),
-              child: CircleAvatar(
-                backgroundImage: NetworkImage(
-                    'https://randomuser.me/api/portraits/women/44.jpg'),
-              ),
-            ),
-            title: Text('Dwight Schrute'),
-            subtitle: Text('Assistant Regional Manager'),
-            trailing: Icon(Icons.more_vert),
-          ),
+        children: [
+          OfficeRoleBar(
+              name: "Michael Scott",
+              officePosition: "Regional Manager",
+              pic: 'https://randomuser.me/api/portraits/men/11.jpg',
+              notificationCount: 1),
+          OfficeRoleBar(
+              name: "Pam Beasley",
+              officePosition: "Receptionist",
+              pic: 'https://randomuser.me/api/portraits/women/22.jpg',
+              notificationCount: 4),
+          OfficeRoleBar(
+              name: "Jim Halpert",
+              officePosition: "Sales Representative",
+              pic: 'https://randomuser.me/api/portraits/men/33.jpg',
+              notificationCount: 129),
+          OfficeRoleBar(
+              name: "Dwight Schrute",
+              officePosition: "Assistant Regional Manager",
+              pic: 'https://randomuser.me/api/portraits/women/44.jpg',
+              notificationCount: 2),
         ],
       ),
+    );
+  }
+}
+
+class OfficeRoleBar extends StatelessWidget {
+  final String name;
+  final String officePosition;
+  final String pic;
+  final int notificationCount;
+  const OfficeRoleBar(
+      {required this.name,
+      required this.officePosition,
+      required this.pic,
+      required this.notificationCount,
+      super.key});
+
+  String formatNotificationCount(int number) {
+    return number > 99 ? '99+' : number.toString();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Badge(
+        label: Text(
+          formatNotificationCount(notificationCount),
+          style: TextStyle(color: Colors.white),
+        ),
+        child: CircleAvatar(
+          backgroundImage: NetworkImage(pic),
+        ),
+      ),
+      title: Text(name),
+      subtitle: Text(officePosition),
+      trailing: Icon(Icons.more_vert),
     );
   }
 }
